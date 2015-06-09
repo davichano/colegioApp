fb84bf9461b6b10f837cf69d3142b7d65844ac4c
mitkev <kchuquilinv@unc.edu.pe>
2015-06-09 11:45:20 -0500

    registro de alumnos

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -16,7 +16,6 @@ public class Alumno implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private String dni;
 
 	private String apellido;
