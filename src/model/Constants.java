301af6066995c74279178f58154f3c316429aa11
mitkev <kchuquilinv@unc.edu.pe>
2015-06-23 10:34:30 -0500

    actuializacion 23/06/2015

---
+++ src/model/Constants.java
@@ -0,0 +1,13 @@
+package model;
+
+public interface Constants {
+	public static int ALUMNO_GROUP = 1;
+    public static int ADMIN_GROUP = 2;
+    public static int APODERADO_GROUP = 3;
+    public static int PROFESOR_GROUP = 4;
+    
+    public static int PENDING_REQUEST = 1;
+    public static int REJECTED_REQUEST = 2;
+    public static int APPROVED_REQUEST = 3;
+
+}
